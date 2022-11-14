ea6a0fb2e7f8c39d6555e0081aa68952ede92d2a
etgoz <etgoz151@gmail.com>
2022-11-14 16:31:42 +0200

    config page and style

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function displayButtonInfo(value){
+    alert(value);
+}
+
+Array.from(document.getElementsByTagName('button')).forEach(button =>{
+    button.onclick = function() {displayButtonInfo(button.value)}
+});
+
 let info = document.getElementById('info');
 info.onclick = function() {
     alert('Developer: Etay Gozlan\nVersion: 1\nThis is a calculator web app')
