8b47e67c7109931a4d64c76a39a69f02e7eb7822
etgoz <etgoz151@gmail.com>
2022-11-15 15:34:46 +0200

    basic operations

--- js/application.js
+++ js/application.js
@@ -1,4 +1,26 @@
-let info = document.getElementById('info');
+const info = document.getElementById('info');
 info.onclick = function() {
     alert('Developer: Etay Gozlan\nVersion: 1\nThis is a calculator web app')
 };
+
+const mode = document.getElementById('mode');
+const body = document.body;
+
+mode.addEventListener('click', function() {
+    if (body.className === "light") {
+        body.className = "dark";
+    } else {
+        body.className = "light";
+    }
+});
+
+const sci = document.getElementById('sci');
+const sciDisplay = document.getElementById('sci-display');
+
+sci.addEventListener('click', function() {
+    if (sciDisplay.style.display === 'none') {
+        sciDisplay.style.display = 'block';
+    } else {
+        sciDisplay.style.display = 'none';
+    }
+});
--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,9 @@
-let cur;
-let a;
-let oprator;
-let b;
+let last;
+let a = '';
+let operator;
+let b = '';
+
+const screen = document.querySelector('#screen');
 
 function add(x, y){
     return x+y;
@@ -19,11 +21,102 @@ function multiply(x, y){
     return x*y;
 }
 
-let input;
+function checkInput(userIn) {
+    if (!operator){
+        if (userIn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]){
+            a += userIn;
+            screen.innerHTML = a;
+        } 
+    } else if (operator){
+        if (userIn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]){
+            b += userIn;
+            screen.innerHTML += b;
+        } 
+    }
+};
 
-Array.from(document.getElementsByClassName('operand')).forEach(button =>{
-    button.onclick = function() {
+let input = '';
+
+Array.from(document.getElementsByClassName('operand')).forEach((button) => {
+    button.addEventListener('click', function() { 
         input = button.value;
-        console.log(input);}
+        checkInput(input);
+        }
+)});
+
+Array.from(document.getElementsByClassName('operator')).forEach((button) => {
+    button.addEventListener('click', function() { 
+        if (!operator){
+            operator = button.value;
+            screen.innerHTML += operator;
+        } else if (operator){
+            switch(operator){
+                case '+':
+                    a = add(Number(a), Number(b));
+                    b = '';
+                    operator = button.value;
+                    break;
+                case '-':
+                    a = sub(Number(a), Number(b));
+                    b = '';
+                    operator = button.value;
+                    break;
+                case 'X':
+                    a = multiply(Number(a), Number(b));
+                    b = '';
+                    operator = button.value;
+                    break;
+                case '/':
+                    a = divide(Number(a), Number(b));
+                    b = '';
+                    operator = button.value;
+                    break;
+            }
+            screen.innerHTML = a + operator;
+        }
+    }
+)});
+
+document.getElementById('equal').addEventListener('click', function(){
+    switch(operator){
+        case '+':
+            last = add(Number(a), Number(b));
+            reset()
+            break;
+        case '-':
+            last = sub(Number(a), Number(b));
+            reset()
+            break;
+        case 'X':
+            last = multiply(Number(a), Number(b));
+            reset()
+            break;
+        case '/':
+            last = divide(Number(a), Number(b));
+            reset()
+            break;
+    }
+    screen.innerHTML = last;
+})
+
+function reset(){
+    a = '';
+    b = '';
+    operator = undefined;
+    screen.innerHTML = ''
+}
+
+document.getElementById('c').addEventListener('click', reset)
+
+document.getElementById('back').addEventListener('click', function (){
+    if (b){
+        b = b.slice(0,-1);
+    } else if (operator){
+        operator = undefined;
+    } else if (a) {
+        a = a.slice(0, -1);
+    }
+    screen.innerHTML = screen.innerHTML.slice(0, -1)
 });
+
 
