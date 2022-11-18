a03bf24a51dc69a00a258bff2154d154c4b54770
etgoz <etgoz151@gmail.com>
2022-11-18 16:15:14 +0200

    fixed buges in calculatins

--- js/calculator.js
+++ js/calculator.js
@@ -1,103 +1,110 @@
 let last;
-let a = '';
+let a = "";
 let operator;
-let b = '';
+let b = "";
 
-const screen = document.querySelector('#screen');
+const screen = document.querySelector("#screen");
 
-function equal(x, oper, y){
-    if (oper === 'X'){
-        oper = '*';
-    }
-    return eval(`${x} ${oper} ${y}`);
-};
-
-function checkInput(userIn) {
-    if (!operator){
-        if (userIn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] || userIn === '.'){
-            a += userIn;
-        } 
-    } else if (operator){
-        if (userIn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] || userIn === '.'){
-            b += userIn;
-        } 
-    }
-};
-
-let input = '';
-
-Array.from(document.getElementsByClassName('operand')).forEach((button) => {
-    button.addEventListener('click', function() { 
-        // if (last){
-        //     reset();
-        //     last = undefined;
-        // }
-        input = button.value;
-        checkInput(input);
-        if(!operator){
-            screen.innerHTML = a.toLocaleString();
-        } else {
-            screen.innerHTML = a.toLocaleString() + operator + b.toLocaleString();
-        }
-    }
-)});
-
-Array.from(document.getElementsByClassName('operator')).forEach((button) => {
-    button.addEventListener('click', function() { 
-        if (!operator){
-            operator = button.value;
-            screen.innerHTML += operator;
-        } else if (operator){
-            if(operator === '/' && b === '0'){
-                a = '';
-                operator = undefined;
-                b = '';
-                screen.innerHTML = 'ERORR';
-            }else{
-                a = equal(a, operator, b);
-                b = '';
-                operator = button.value;
-                screen.innerHTML = a.toLocaleString() + operator;
-            }}
-        }
-)});
-
-document.getElementById('equal').addEventListener('click', function(){
-    if (!operator){
-        last = a;
-        a = '';
-        screen.innerHTML = last.toLocaleString();
-    } else if (operator){
-        if(operator === '/' && b === '0'){
-            a = '';
-            operator = undefined;
-            b = '';
-            screen.innerHTML = 'ERORR';
-        }else{
-            last = equal(a, operator, b);
-            b = '';
-            operator = undefined;
-            a = last;
-            screen.innerHTML = a.toLocaleString();
-        }}  
-})
-
-function reset(){
-    a = '';
-    b = '';
-    operator = undefined;
-    screen.innerHTML = ''
+function equal(x, oper, y) {
+	if (oper === "X") {
+		oper = "*";
+	}
+	return eval(`${x} ${oper} ${y}`);
 }
 
-document.getElementById('c').addEventListener('click', reset);
+function checkInput(userIn) {
+	if (!operator) {
+		if (userIn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] || userIn === ".") {
+			a += userIn;
+		}
+	} else if (operator) {
+		if (userIn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] || userIn === ".") {
+			b += userIn;
+		}
+	}
+}
 
-document.getElementById('back').addEventListener('click', function (){
-    if (b){
-        b = b.slice(0,-1);
-    } else if (operator){
-        operator = undefined;
-    } else if (a) {
-        a = a.slice(0, -1);
-    }
-    screen.innerHTML = screen.innerHTML.slice(0, -1)
+let input = "";
+
+Array.from(document.getElementsByClassName("operand")).forEach((button) => {
+	button.addEventListener("click", function () {
+		// if (last){
+		//     reset();
+		//     last = undefined;
+		// }
+		input = button.value;
+		checkInput(input);
+		if (!operator) {
+			screen.innerHTML = a.toLocaleString();
+		} else {
+			screen.innerHTML = a.toLocaleString() + operator + b.toLocaleString();
+		}
+	});
+});
+
+Array.from(document.getElementsByClassName("operator")).forEach((button) => {
+	button.addEventListener("click", function () {
+		if (!operator) {
+			operator = button.value;
+			screen.innerHTML += operator;
+		} else if (operator) {
+			if (operator === "/" && b === "0") {
+				a = "";
+				operator = undefined;
+				b = "";
+				screen.innerHTML = "ERORR";
+			} else {
+				if (b) {
+					a = equal(a, operator, b);
+					b = "";
+					operator = button.value;
+					screen.innerHTML = a.toLocaleString() + operator;
+				} else {
+					operator = button.value;
+					screen.innerHTML = a.toLocaleString() + operator;
+				}
+			}
+		}
+	});
+});
+
+document.getElementById("equal").addEventListener("click", function () {
+	if (!operator) {
+		last = a;
+		a = "";
+		screen.innerHTML = last.toLocaleString();
+	} else if (operator) {
+		if (operator === "/" && b === "0") {
+			a = "";
+			operator = undefined;
+			b = "";
+			screen.innerHTML = "ERORR";
+		} else {
+			last = equal(a, operator, b);
+			b = "";
+			operator = undefined;
+			a = last;
+			screen.innerHTML = a.toLocaleString();
+		}
+	}
+});
+
+function reset() {
+	a = "";
+	b = "";
+	operator = undefined;
+	screen.innerHTML = "";
+}
+
+document.getElementById("c").addEventListener("click", reset);
+
+document.getElementById("back").addEventListener("click", function () {
+	if (b) {
+		b = b.slice(0, -1);
+	} else if (operator) {
+		operator = undefined;
+	} else if (a) {
+		a = a.slice(0, -1);
+	}
+	screen.innerHTML = screen.innerHTML.slice(0, -1);
 });
